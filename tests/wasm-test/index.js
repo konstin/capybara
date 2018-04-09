10d8a63cdc28644ab266e6b36fd257761958d040
konstin <konstin@mailbox.org>
2018-04-09 22:59:57 +0200

    Use pseudo-constructors for wasm
    wasm-bindgen doesn't support the usage of javascript "new SomeClass"
    style constructors, but SomeClass.new() is sufficient for now

--- tests/wasm-test/index.js
+++ tests/wasm-test/index.js
@@ -2,3 +2,5 @@ const {MyClass} = require("./wasm_test");
 
 let x = MyClass.add_and_print(21, 21);
 console.log(x);
+let instance = MyClass.new(42);
+console.log(instance.get_number());
--- tests/wasm-test/src/lib.rs
+++ tests/wasm-test/src/lib.rs
@@ -7,13 +7,25 @@ extern crate capybara;
 use capybara::capybara_bindgen;
 
 #[capybara_bindgen]
-pub struct MyClass {}
+pub struct MyClass {
+    number: i32,
+}
 
 #[capybara_bindgen]
 impl MyClass {
+    pub fn new(number: i32) -> MyClass {
+        MyClass {
+            number
+        }
+    }
+
     pub fn add_and_print(x: i32, y: i32) -> i32 {
         println!("Printing from rust: {}", x + y);
         x + y
+    }
+
+    pub fn get_number(&self) -> i32 {
+        self.number
     }
 }
 
